89c60874a1b9346f6291f7698dab897776f50e92
Jaime <jaramillonz16@gmail.com>
2023-08-10 22:51:53 -0400

    svg

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ export const Footer = () => {
                 <div className="container py-2">
                     <div className="row gy-4 gx-5">
                         <div className="col-lg-4 col-md-5">
-                            <NavLink className="" to="/"><img src="public/logo-footer.png" alt="logo" className="img-fluid" /></NavLink>
+                            <NavLink className="" to="/"><img src="images/logo-footer.png" alt="logo" className="img-fluid" /></NavLink>
                             <br />
                             <p className="small text-muted">Somo el restaurante N°1 en pollos</p>
                         </div>
